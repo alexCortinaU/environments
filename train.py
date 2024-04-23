46cac4d03b52bca2615727d44761e4fd0dd8af8a
Alejandro Cortina <101715739+alexCortinaU@users.noreply.github.com>
2024-04-23 14:23:56 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os
+import pathlib as Path
 import argparse
 from torch import utils
 from torch import cuda
@@ -7,6 +8,7 @@ from torchvision.transforms import ToTensor
 import lightning as L
 
 from src.models.dummy import LitAutoEncoder
+this_path = Path.cwd()
 
 
 parser = argparse.ArgumentParser()
@@ -23,7 +25,8 @@ def train(limit_train_batches=100,
           gpu_idx=None):
 
     # setup data
-    dataset = MNIST(os.getcwd(), download=True, transform=ToTensor())
+    mnist_path = Path('/LovbeskyttetMapper/PERSIMUNE-1401-XRAY/rt-ssl/data/')
+    dataset = MNIST(mnist_path, download=True, transform=ToTensor())
     train_loader = utils.data.DataLoader(dataset)
 
     # init the autoencoder
@@ -44,6 +47,7 @@ def train(limit_train_batches=100,
 
 
 if __name__ == '__main__':
+    print(this_path)
     limit_train_batches = args.limit_train_batches
     max_epochs = args.max_epochs
     print('Starting training with: \n')
@@ -63,4 +67,6 @@ if __name__ == '__main__':
           max_epochs=max_epochs,
           accelerator=accelerator,
           gpu_idx=gpu_idx)
+    
+
     
